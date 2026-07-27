7de92608a027ec7569cf981d36160fcdbc910cf9
JHTheisen <107666198+JHTheisen@users.noreply.github.com>
2026-07-27 19:34:12 -0400

    Add I2C scanner functionality in i2c_scanner.ino

---
+++ i2c_scanner/i2c_scanner.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+void setup() {
+  Wire.begin(SDA_PIN, SCL_PIN);
+  Serial.begin(115200);
+
+  while (!Serial); // wait for serial monitor
+  Serial.println("\nI2C Scanner");
+}
+
+void loop() {
+  byte error, address;
+  int nDevices = 0;
+
+  Serial.println("Scanning...");
+
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println(" !");
+      nDevices++;
+    }
+    else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("Done\n");
+
+  delay(5000); // wait 5 seconds before next scan
+}
